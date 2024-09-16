f7b8d28eff71dc6dea0ccb6864d8ae980d94e0d5
Simo Katopera <simo.katopera@hotmail.com>
2024-09-16 22:26:39 +0300

    Functions from html to ts-file

--- dist/chartfunctions.js
+++ dist/chartfunctions.js
@@ -1,37 +1,68 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.formatReadingsTS = void 0;
-function formatReadingsTS(readings) {
+exports.formatFilteredTableTS = exports.filterSeriesTS = void 0;
+function createHalfFilledFiltered(value, date, first, last) {
+    return { index: -1, value: value, morning: NaN, evening: NaN, date: date, firstday: first, lastday: last };
+}
+function filterSeriesTS(serie, filterlength) {
+    let firstindex = 0;
+    let lastindex = 0;
+    let negoffset = filterlength % 2 ? -filterlength / 2 + 0.5 : -filterlength / 2;
+    let posoffset = negoffset + filterlength;
+    let filtered = serie.map(ss => {
+        if (!isNaN(ss.value)) {
+            let first = new Date(ss.date.getFullYear(), ss.date.getMonth(), ss.date.getDate() + negoffset);
+            let last = new Date(ss.date.getFullYear(), ss.date.getMonth(), ss.date.getDate() + posoffset);
+            while (firstindex < serie.length && serie[firstindex].date < first)
+                firstindex++;
+            while (lastindex < serie.length && serie[lastindex].date < last)
+                lastindex++;
+            let sum = 0;
+            let dec = 0;
+            for (let index = firstindex; index < lastindex; index++) {
+                if (isNaN(serie[index].value))
+                    dec++;
+                else
+                    sum += serie[index].value;
+            }
+            return createHalfFilledFiltered(sum / (lastindex - firstindex - dec), ss.date, first, last);
+        }
+        return createHalfFilledFiltered(NaN, ss.date, null, null);
+    });
+    return filtered;
+}
+exports.filterSeriesTS = filterSeriesTS;
+function formatFilteredTableTS(readings, filtered) {
     let index;
     let dayindex = 0;
     let yearindex = 0;
-    for (index = 0; index < readings.filtered.length && yearindex < readings.values.length; index++) {
-        if (readings.filtered[index].date < readings.values[yearindex].data[dayindex].datetimeLocal) {
-            readings.filtered[index].morning = NaN;
-            readings.filtered[index].evening = NaN;
+    for (index = 0; index < filtered.length && yearindex < readings.length; index++) {
+        if (filtered[index].date < readings[yearindex].data[dayindex].datetimeLocal) {
+            filtered[index].morning = NaN;
+            filtered[index].evening = NaN;
         }
         else {
-            let y1 = readings.filtered[index].date.getFullYear();
-            let m1 = readings.filtered[index].date.getMonth();
-            let d1 = readings.filtered[index].date.getDate();
-            let y2 = readings.values[yearindex].data[dayindex].datetimeLocal.getFullYear();
-            let m2 = readings.values[yearindex].data[dayindex].datetimeLocal.getMonth();
-            let d2 = readings.values[yearindex].data[dayindex].datetimeLocal.getDate();
+            let y1 = filtered[index].date.getFullYear();
+            let m1 = filtered[index].date.getMonth();
+            let d1 = filtered[index].date.getDate();
+            let y2 = readings[yearindex].data[dayindex].datetimeLocal.getFullYear();
+            let m2 = readings[yearindex].data[dayindex].datetimeLocal.getMonth();
+            let d2 = readings[yearindex].data[dayindex].datetimeLocal.getDate();
             if (y1 == y2 && m1 == m2 && d1 == d2) {
-                readings.filtered[index].morning = readings.values[yearindex].data[dayindex].morning;
-                readings.filtered[index].evening = readings.values[yearindex].data[dayindex].evening;
+                filtered[index].morning = readings[yearindex].data[dayindex].morning;
+                filtered[index].evening = readings[yearindex].data[dayindex].evening;
                 dayindex++;
-                if (dayindex >= readings.values[yearindex].data.length) {
+                if (dayindex >= readings[yearindex].data.length) {
                     dayindex = 0;
                     yearindex++;
                 }
             }
             else {
-                while (yearindex < readings.values.length && dayindex < readings.values[yearindex].data.length) {
-                    readings.filtered[index].morning = NaN;
-                    readings.filtered[index].evening = NaN;
+                while (yearindex < readings.length && dayindex < readings[yearindex].data.length) {
+                    filtered[index].morning = NaN;
+                    filtered[index].evening = NaN;
                     dayindex++;
-                    if (dayindex > readings.values[yearindex].data.length) {
+                    if (dayindex > readings[yearindex].data.length) {
                         dayindex = 0;
                         yearindex++;
                     }
@@ -39,18 +70,18 @@ function formatReadingsTS(readings) {
             }
         }
     }
-    readings.filtered = readings.filtered.reverse();
+    filtered = filtered.reverse();
     index = 0;
-    while (index < readings.filtered.length && isNaN(readings.filtered[index].value))
+    while (index < filtered.length && isNaN(filtered[index].value))
         index++;
     if (index > 0)
-        readings.filtered.splice(0, index);
-    index = readings.filtered.length - 1;
-    while (index < readings.filtered.length && isNaN(readings.filtered[index].value))
+        filtered.splice(0, index);
+    index = filtered.length - 1;
+    while (index < filtered.length && isNaN(filtered[index].value))
         index--;
     if (index > 0)
-        readings.filtered.splice(index + 1);
-    readings.filtered.forEach((r, i) => { r.index = i; });
-    return readings;
+        filtered.splice(index + 1);
+    filtered.forEach((r, i) => { r.index = i; });
+    return filtered;
 }
-exports.formatReadingsTS = formatReadingsTS;
+exports.formatFilteredTableTS = formatFilteredTableTS;
--- server/chartfunctions.ts
+++ server/chartfunctions.ts
@@ -20,39 +20,85 @@ interface Filtered {
     firstday: Date;
     lastday: Date;
 }
+function createHalfFilledFiltered(value: number, date: Date, first: Date, last: Date): Filtered {
+    return {index: -1, value: value, morning: NaN, evening: NaN, date: date, firstday: first, lastday: last}
+}
+interface Temperature {
+    date: Date;
+    value: number;
+    average: number;
+}
+interface DbTemperature {
+    data: DbData[];
+    info: Info;
+}
+interface Info {
+    location: string;
+    year: number;
+}
+interface DbData {
+    date: string;
+    morning: number;
+    evening: number;
+    datetimeLocal: Date;
+    datetimeUtc: Date;
+}
+export function filterSeriesTS(serie: Temperature[], filterlength: number): Filtered[] {
+    let firstindex: number = 0;
+    let lastindex: number = 0;
+    let negoffset: number = filterlength % 2 ? -filterlength / 2 + 0.5 : -filterlength / 2;
+    let posoffset: number = negoffset + filterlength;
+    let filtered: Filtered[] = serie.map(ss => {
+        if (!isNaN(ss.value)) {
+            let first = new Date(ss.date.getFullYear(), ss.date.getMonth(), ss.date.getDate() + negoffset);
+            let last = new Date(ss.date.getFullYear(), ss.date.getMonth(), ss.date.getDate() + posoffset);
+            while (firstindex < serie.length && serie[firstindex].date < first) firstindex++;
+            while (lastindex < serie.length && serie[lastindex].date < last) lastindex++;
+            let sum: number = 0;
+            let dec: number = 0;
+            for (let index: number = firstindex; index < lastindex; index++) {
+                if (isNaN(serie[index].value)) dec++;
+                else sum += serie[index].value;
+            }
+            return createHalfFilledFiltered( sum / (lastindex - firstindex - dec), ss.date, first, last );
+        }
+        return createHalfFilledFiltered( NaN, ss.date, null, null);
+    })
+    return filtered;
+}
 
-export function formatReadingsTS(readings: FilteredReadings): FilteredReadings {
+export function formatFilteredTableTS(readings: DbTemperature[], filtered: Filtered[]): Filtered[] {
     let index: number;
     let dayindex: number = 0;
     let yearindex: number = 0;
-    for (index = 0; index < readings.filtered.length && yearindex < readings.values.length; index++) {
-        if (readings.filtered[index].date < readings.values[yearindex].data[dayindex].datetimeLocal) {
-            readings.filtered[index].morning = NaN;
-            readings.filtered[index].evening = NaN;
+    for (index = 0; index < filtered.length && yearindex < readings.length; index++) {
+        if (filtered[index].date < readings[yearindex].data[dayindex].datetimeLocal) {
+            filtered[index].morning = NaN;
+            filtered[index].evening = NaN;
         }
         else {
-            let y1 = readings.filtered[index].date.getFullYear();
-            let m1 = readings.filtered[index].date.getMonth();
-            let d1 = readings.filtered[index].date.getDate();
-            let y2 = readings.values[yearindex].data[dayindex].datetimeLocal.getFullYear();
-            let m2 = readings.values[yearindex].data[dayindex].datetimeLocal.getMonth();
-            let d2 = readings.values[yearindex].data[dayindex].datetimeLocal.getDate();
+            let y1 = filtered[index].date.getFullYear();
+            let m1 = filtered[index].date.getMonth();
+            let d1 = filtered[index].date.getDate();
+            let y2 = readings[yearindex].data[dayindex].datetimeLocal.getFullYear();
+            let m2 = readings[yearindex].data[dayindex].datetimeLocal.getMonth();
+            let d2 = readings[yearindex].data[dayindex].datetimeLocal.getDate();
 
             if (y1 == y2 && m1 == m2 && d1 == d2) {
-                readings.filtered[index].morning = readings.values[yearindex].data[dayindex].morning;
-                readings.filtered[index].evening = readings.values[yearindex].data[dayindex].evening;
+                filtered[index].morning = readings[yearindex].data[dayindex].morning;
+                filtered[index].evening = readings[yearindex].data[dayindex].evening;
                 dayindex++;
-                if (dayindex >= readings.values[yearindex].data.length) {
+                if (dayindex >= readings[yearindex].data.length) {
                     dayindex = 0;
                     yearindex++;
                 }
             }
             else {
-                while (yearindex < readings.values.length && dayindex < readings.values[yearindex].data.length) {
-                    readings.filtered[index].morning = NaN;
-                    readings.filtered[index].evening = NaN;
+                while (yearindex < readings.length && dayindex < readings[yearindex].data.length) {
+                    filtered[index].morning = NaN;
+                    filtered[index].evening = NaN;
                     dayindex++;
-                    if (dayindex > readings.values[yearindex].data.length) {
+                    if (dayindex > readings[yearindex].data.length) {
                         dayindex = 0;
                         yearindex++;
                     }
@@ -60,15 +106,17 @@ export function formatReadingsTS(readings: FilteredReadings): FilteredReadings {
             }
         }
     }
-    readings.filtered = readings.filtered.reverse();
+    filtered = filtered.reverse();
     // remove empty values from the beginning and end
     index = 0;
-    while (index < readings.filtered.length && isNaN(readings.filtered[index].value)) index++;
-    if (index > 0) readings.filtered.splice(0, index);
-    index = readings.filtered.length - 1;
-    while (index < readings.filtered.length && isNaN(readings.filtered[index].value)) index--;
-    if (index > 0) readings.filtered.splice(index + 1);
+    while (index < filtered.length && isNaN(filtered[index].value)) index++;
+    if (index > 0) filtered.splice(0, index);
+    index = filtered.length - 1;
+    while (index < filtered.length && isNaN(filtered[index].value)) index--;
+    if (index > 0) filtered.splice(index + 1);
     // add index
-    readings.filtered.forEach((r, i) => {r.index = i;})
-    return readings;
+    filtered.forEach((r, i) => {r.index = i;});
+
+    return filtered;
 }
+
